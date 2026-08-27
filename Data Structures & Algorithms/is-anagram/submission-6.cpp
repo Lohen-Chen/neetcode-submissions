95eddcb1ce1d1ea1a300fcd96e5eee06c654742b
Lohen Chen <64998077+Lohen-Chen@users.noreply.github.com>
2026-08-27 11:46:24 -0400

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.cpp
@@ -0,0 +1,21 @@
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        int counts[26] = {0};
+        for(int i = 0; i < s.length(); i++){
+            counts[s[i] - 'a']--;
+            counts[t[i] - 'a']++;
+        }
+
+        for(const auto& num : counts){
+            if(num != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
